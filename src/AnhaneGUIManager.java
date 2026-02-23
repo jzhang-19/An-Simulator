70844d7892d272d356ff54560ba994c8729f3e9a
jzhang-19 <justinzh23@gmail.com>
2026-02-22 20:21:26 -0800

    finally should work

--- src/AnhaneGUIManager.java
+++ src/AnhaneGUIManager.java
@@ -5,6 +5,8 @@ import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.nio.file.Paths;
+
+import java.net.URL;
 import javax.swing.ImageIcon;
 import java.io.File;
 
@@ -39,27 +41,35 @@ public class AnhaneGUIManager {
 
     }
 
-    public ArrayList<ImageIcon> addAllImages(){
+    public ArrayList<ImageIcon> addAllImages() {
         privList.clear();
-
-        String folderPath = Paths.get("src","images").toString();
-        File folder = new File(folderPath);
-        File[] files = folder.listFiles();
-
-        if (files != null){
-            for (File f: files){
-                if (f.isFile() && isImageFile(f.getName())){
-                    privList.add(new ImageIcon(f.getAbsolutePath()));
+    
+        try {
+            URL folderURL = getClass().getResource("/images");
+            if (folderURL == null) {
+                System.out.println("Images folder not found!");
+                return privList;
+            }
+    
+            File folder = new File(folderURL.toURI());
+            File[] files = folder.listFiles();
+    
+            if (files != null) {
+                for (File f : files) {
+                    if (f.isFile() && isImageFile(f.getName())) {
+                        URL imgURL = getClass().getResource("/images/" + f.getName());
+                        privList.add(new ImageIcon(imgURL)); 
+                    }
                 }
             }
+    
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        else{
-            System.out.println("Images folder is empty");
-        }
+    
         return privList;
-        
-        
     }
+
     public boolean isImageFile(String name){
         name = name.toLowerCase();
         if (name.endsWith(".png") || name.endsWith(".jpg")|| name.endsWith(".jpeg")){
